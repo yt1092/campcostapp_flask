38ccd09d75b839e5bd99ca3d124ab47f7b5e7039
yt1092 <194182518+yt1092@users.noreply.github.com>
2025-10-17 18:06:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,6 @@ def calculate():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render が指定するポートを取得
+    app.run(host="0.0.0.0", port=port, debug=True)
