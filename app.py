13797d074c2cb3dbe9438094b8c1b21915d4f0fe
yt1092 <194182518+yt1092@users.noreply.github.com>
2025-10-18 17:52:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -71,4 +71,6 @@ def calculate():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
